1cee19a08e55d08926a37f31cda44a9cd1175836
ricardo540 <ricardo.izasiga@gmail.com>
2018-07-01 17:26:59 -0500

    add_nav

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def short_name
+    "Ricardo Garcia"
+  end
 end
